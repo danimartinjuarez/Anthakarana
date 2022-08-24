57170d01957e68859d2df5654ee73f56197fa2b8
David <d.barcia9@gmail.com>
2022-08-24 11:28:16 +0200

    Tarjetas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+@foreach ($events -> $event){
+<div class="card bg-dark text-white">
+  <img class="card-img" src= "{{ $event -> image }}" alt="Card image">
+  <div class="card-img-overlay">
+    <h5 class="card-title">{{$event -> title}}</h5>
+    <p class="card-text">{{$event -> date}}/p>
+    
+  </div>
 </div>
+
+}
+@endforeach
 @endsection
