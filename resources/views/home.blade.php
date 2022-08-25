76147ff62ccb0192427a45d3369d16878eab3303
dani martin <danimartinjuarez@gmail.com>
2022-08-25 18:16:31 +0200

    Order cards and elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,26 @@
     @foreach ($events as $event)
     <div class="card bg-dark text-white">
         <img class="card-img" src="{{ $event -> image }}" alt="Card image">
-        <div class="card-img-overlay">
+        <div class="card-img-overlay" id="card-image">
             <h5 class="card-title">{{$event -> title}}</h5>
             <p class="card-text">{{$event -> date}}</p>
-            <div class="card-footer bottom-0">
-                <div>
+            <div class="row-cols-md-3 g-4" id="card-footer">
+                <div class="card-title " id="carrousel-button">
                     <label class="form-check-label" for="flexCheckDefault">
                         carrusel
                     </label>
+
                     <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
                 </div>
-                <button type="button" class="btn btn-primary">Asistir</button>
+                <button type="button" class="btn btn-primary" id="asist-button">Asistir</button>
+                <form action="{{ route('delete', ['id'=>$event->id]) }}" method="post">
+                    @method ('delete')
+                    @csrf
+                    <button type="submit" name="delete" class="btn btn-primary"
+                        onclick="return confirm('Estas seguro que quieres borrar? {{$event->name}}  {{$event->id}}')">
+                        <img
+                            src="htps://www.ordenencasa.shop/wp-content/uploads/2020/05/papelera-redonda-en-madera-de-bambu.jpg" />
+                    </button>
             </div>
         </div>
     </div>
