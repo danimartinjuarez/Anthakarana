90023f22e401aa99c95a81f8dd02787f1fa96c44
Patricia Rodríguez <prlongoria@gmail.com>
2022-08-26 05:59:29 +0200

    insert 3 real events

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Event::factory()->create(['title'=>'Abrazar árboles ', 'description'=>'Actividad muy relajante y armonizadora de energías', 'people'=>'25', 'image'=>'https://st.depositphotos.com/1518767/4293/i/950/depositphotos_42935891-stock-photo-boy-hugging-a-tree-at.jpg', 'date'=>'2022-08-14 08:00:00']);
+        Event::factory()->create(['title'=>'Iniciación Reiki I', 'description'=>'Te permitirá tratarte a ti y a quien lo necesite', 'image'=>'https://th.bing.com/th/id/OIP.WhSVeVwJcJEFSoZ0eH51OwHaHa?pid=ImgDet&rs=1', 'people'=>'7']);
+        Event::factory()->create(['title'=>'Retiro Espiritual', 'description'=>'Si necesitas desconectar esta es tu actividad ideal ', 'image'=>'https://th.bing.com/th/id/R.fde987fa010bb3fc89eb38cc1d6cfefa?rik=%2beHb%2fOY9ja6GzQ&pid=ImgRaw&r=0', 'people'=>'30']);
         Event::factory(10)->create();
     }
 }
