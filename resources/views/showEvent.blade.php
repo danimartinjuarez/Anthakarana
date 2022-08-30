e34aa3c47997884e2add73f22850ded33024b4f7
silviacoder <silviasgpg@gmail.com>
2022-08-30 11:28:40 +0200

    card in showevent

--- resources/views/showEvent.blade.php
+++ resources/views/showEvent.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-
+<div class="card text-bg-dark">
+  <img src="{{ $event->image }}" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">"{{$event -> title}}"</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
 
 
 @endsection
