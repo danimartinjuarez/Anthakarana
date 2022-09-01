ac378d3fc687b1e697da6fd1b33894a9e271f025
sudoBuda <juanfranciscobalseca@gmail.com>
2022-09-01 11:08:03 +0200

    first changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
 </div>
 @endsection('carousel')
 @section('content')
-<div>
+<div class="container">
     <div class="row-cols-1 row-cols-md-3">
         <a class=" d-inline-flex justify-content-center gap-2 link-unstyled" href="{{route ('createEvent')}}">
             <p>New Event</p>
@@ -52,13 +52,11 @@
     </div>
     <div class="row row-cols-1 row-cols-md-4 g-4 gap-4 justify-content-center mx-5">
         @foreach ($events as $event)
-        @if ($event->date < (now()) )
-        
-            <div class="card bg-dark text-white">
+        @if ($event->date < (now()) ) <div class="card bg-dark text-white">
             <img class="card-img img-fluid h-100 d-flex" src="{{ $event -> image }}" alt="Card image">
             <div class="card-img-overlay overlay d-flex bg-dark bg-opacity-75">
                 </img>
-                
+
                 <h3 class="text-white">EVENTO PASADO</h3>
                 <div class="w-75 h-10 d-flex flex-column align-self-end text-white">
                     <h5 class="card-title">{{$event -> title}}</h5>
@@ -86,45 +84,45 @@
                     <a href="{{ route('showEvent', $event->id) }}" class="text-white">Ver</a>
                 </div>
             </div>
-        </div>
-        @endif
-        @if ($event->date > (now()))
-        <div class="card bg-dark text-white">
-            <img class="card-img img-fluid h-100 d-flex" src="{{ $event -> image }}" alt="Card image">
-            <div class="card-img-overlay overlay d-flex">
-                </img>
-                <div class="w-75 h-10 d-flex flex-column align-self-end">
-                
-                    <h5 class="card-title text-white">{{$event -> title}}</h5>
-                    <p class="card-text text-white">{{$event -> date}}</p>
-                </div>
+    </div>
+    @endif
+    @if ($event->date > (now()))
+    <div class="card bg-dark text-white">
+        <img class="card-img img-fluid h-100 d-flex" src="{{ $event -> image }}" alt="Card image">
+        <div class="card-img-overlay overlay d-flex">
+            </img>
+            <div class="w-75 h-10 d-flex flex-column align-self-end">
 
-                <div class="w-25 h-15 d-flex flex-column align-self-end align-items-end">
-                    <!-- <div class=""> -->
-                    <!-- <label class="form-check-label" for="flexCheckDefault">Carrusel</label>
+                <h5 class="card-title text-white">{{$event -> title}}</h5>
+                <p class="card-text text-white">{{$event -> date}}</p>
+            </div>
+
+            <div class="w-25 h-15 d-flex flex-column align-self-end align-items-end">
+                <!-- <div class=""> -->
+                <!-- <label class="form-check-label" for="flexCheckDefault">Carrusel</label>
                         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
                     </div> -->
-                    <a href="{{ route('showEvent', $event->id) }}" class="text-white">Ver</a>
-                    <button type="button" class="btn btn-primary buttonAsist" id="asist-button"><a href="{{ route('inscribeEvent', ['id'=>$event->id]) }}">Asistir</a></button>
+                <a href="{{ route('showEvent', $event->id) }}" class="text-white">Ver</a>
+                <button type="button" class="btn btn-primary text-reset buttonAsist"><a href="{{ route('inscribeEvent', ['id'=>$event->id]) }}">Asistir</a></button>
 
-                    @if (Auth::check() && Auth::user()->isAdmin)
+                @if (Auth::check() && Auth::user()->isAdmin)
 
-                    <form action="{{ route('delete', ['id' => $event->id]) }}" method="post" class="erase-button">
-                        @method('delete')
-                        @csrf
-                        <button type="submit" class="bt-adm m-1 d-flex justify-content-center align-items-center" onclick="return confirm('¿Estás seguro de querer eliminar este evento? {{$event->name}} -ID {{ $event -> id }}')">
-                            <img class="erase-img" src=" {{url('/img/DeleteButtonIcon.png')}}">
-                        </button>
-                        <a href="{{ route('editEvent', ['id'=>$event->id]) }}">Editar</a>
-                    </form>
-                    @endif
-                    
-                </div>
+                <form action="{{ route('delete', ['id' => $event->id]) }}" method="post" class="erase-button">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="bt-adm m-1 d-flex justify-content-center align-items-center" onclick="return confirm('¿Estás seguro de querer eliminar este evento? {{$event->name}} -ID {{ $event -> id }}')">
+                        <img class="erase-img" src=" {{url('/img/DeleteButtonIcon.png')}}">
+                    </button>
+                    <a href="{{ route('editEvent', ['id'=>$event->id]) }}">Editar</a>
+                </form>
+                @endif
+
             </div>
         </div>
-         @endif
-
-        @endforeach
     </div>
+    @endif
+
+    @endforeach
+</div>
 </div>
 @endsection
