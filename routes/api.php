9e3aae1fa66a0bd69636025b542867fbcc890173
Patricia Rodríguez <prlongoria@gmail.com>
2022-09-04 12:55:17 +0200

    api read, delete and tests

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', [EventController::class, 'index'])->name('eventsApi');
+Route::delete('/deleteEvent/{id}', [EventController::class, 'destroy'])->name('destroyEventApi');
