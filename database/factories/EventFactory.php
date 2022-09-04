ae7c2de0c162123d3c54aef1b4e83685cc96a7e6
sudoBuda <juanfranciscobalseca@gmail.com>
2022-09-04 16:36:33 +0200

    styles finished and code formated

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -9,22 +9,22 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class EventFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            //
-            'title' => $this->faker->company(),
-            'total_people' => $this->faker->biasedNumberBetween($min = 1, $max = 50, $function = 'sqrt'),
-            'sub_people' => $this->faker->biasedNumberBetween($min = 1, $max = 50, $function = 'sqrt'),
-            'description' => $this->faker->company(),
-            'image' => $this->faker->imageUrl(),
-            'date' => $this->faker->date($format = 'Y-m-d', $min = 'now', $timezone = null),
-            'start_hour' => $this->faker->time($format = 'H:i', $startTime = 'now', ),
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+			//
+			'title' => $this->faker->company(),
+			'total_people' => $this->faker->biasedNumberBetween($min = 1, $max = 50, $function = 'sqrt'),
+			'sub_people' => $this->faker->biasedNumberBetween($min = 1, $max = 50, $function = 'sqrt'),
+			'description' => $this->faker->company(),
+			'image' => $this->faker->imageUrl(),
+			'date' => $this->faker->date($format = 'Y-m-d', $min = 'now', $timezone = null),
+			'start_hour' => $this->faker->time($format = 'H:i', $startTime = 'now',),
+		];
+	}
 }
