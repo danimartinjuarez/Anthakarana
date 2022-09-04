b628281c5436e348f8b8e45ceaff801a720a7e34
dani martin <danimartinjuarez@gmail.com>
2022-09-04 12:55:07 +0200

    minor fix in calcel button of delete

--- resources/views/showEvent.blade.php
+++ resources/views/showEvent.blade.php
@@ -5,18 +5,18 @@
 
 
     <div class="card-img-overlay row g-3 col-md-5 ">
-      
-    <div class="p-3 mb-2 bg-warning text-dark">
-      <img class=" erase-img" src=" {{url('/img/InfoIcon.png')}}">
-      
-      <span class="fs-4 fw-bold">Información del Evento</span>
-    
-    </div>
+
+        <div class="p-3 mb-2 bg-warning text-dark">
+            <img class=" erase-img" src=" {{url('/img/InfoIcon.png')}}">
+
+            <span class="fs-4 fw-bold">Información del Evento</span>
+
+        </div>
         <h5 class="card-title">Evento:</h5>
         <p class="card-title">{{$event -> title}}</p>
         <h5 class="card-title">Descripción:</h5>
         <p class="card-text">{{ $event -> description }}</p>
-        
+
 
         <div class="d-flex gap-5">
             <h5 class="card-title">Fecha Inicio:</h5>
@@ -44,7 +44,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <img src="{{url('/img/Logo.png')}}" alt="Logo">
+                        <img src="{{url('/img/Logo.png')}}" alt="Logo">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -57,7 +57,9 @@
                             <button type="submit" class="bt-adm m-1 d-flex justify-content-center align-items-center">
                                 <img class="erase-img" src=" {{url('/img/DeleteButtonIcon.png')}}">
                             </button>
-                            <button class="btn-warning" type="cancel" >CANCEL</button>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">CANCEL</span>
+                            </button>
                         </form>
                     </div>
 
@@ -75,73 +77,83 @@
                     class="btn btn-primary buttonAsist text-white" onclick="('Evento completo')"
                     id="asist-button">Asistir</button></a>
 
-                        @endif
-                </form>
-                    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Editar
-</button>
-<!-- Modal -->
+            @endif
+            </form>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Editar
+            </button>
+            <!-- Modal -->
 
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header p-3 mb-2 bg-warning text-dark">
-        <img src="/img/EditButonIcon.png" alt="Boton de editar">
-        <h5 class="modal-title" id="exampleModalLabel">Editar el evento</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form class="justify-content-center col-md-3 m-5" action="{{ route('eventupdate', ['id'=>$event->id]) }}" method="post">
-  @method('PATCH')
-  @csrf
-  
-  
-  <div class="form-group ">
- 
-    
-    <label for="exampleFormControlInput1">Nombre</label>
-    <input type="text" name="title" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> title }}">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Descripcion</label>
-    <input type="text" name="description" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> description }}">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">nº Plazas</label>
-    <input type="number" name="total_people" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> total_people }}">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">URL Imagen</label>
-    <input type="text" name="image" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> image }}">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Fecha del evento</label>
-    <input type="date" name="date" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> date }}">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Hora del evento</label>
-    <input type="time" name="start_hour" class="form-contro" id="exampleFormControlInput1" value="{{ $event -> start_hour }}">
-  </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header p-3 mb-2 bg-warning text-dark">
+                            <img src="/img/EditButonIcon.png" alt="Boton de editar">
+                            <h5 class="modal-title" id="exampleModalLabel">Editar el evento</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 
-  <div class="d-flex flex-row bd-highlight mb-3 p-4">
-    <a class="btn btn-primary" href="{{ route('showEvent', $event -> id) }}">Cancelar</a>
-    <button type="submit" class="btn btn-outline-success" value="Create"  onclick="return confirm('¿Estás seguro de querer modificar este evento? {{$event->name}}')">Aceptar</button>
-  </div>
-  
-   
-  </div>
-  
-  </div>
-</form>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="justify-content-center col-md-3 m-5"
+                                action="{{ route('eventupdate', ['id'=>$event->id]) }}" method="post">
+                                @method('PATCH')
+                                @csrf
 
-   
-  </div>
-</div>
-                   
+
+                                <div class="form-group ">
+
+
+                                    <label for="exampleFormControlInput1">Nombre</label>
+                                    <input type="text" name="title" class="form-contro" id="exampleFormControlInput1"
+                                        value="{{ $event -> title }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Descripcion</label>
+                                    <input type="text" name="description" class="form-contro"
+                                        id="exampleFormControlInput1" value="{{ $event -> description }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">nº Plazas</label>
+                                    <input type="number" name="total_people" class="form-contro"
+                                        id="exampleFormControlInput1" value="{{ $event -> total_people }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">URL Imagen</label>
+                                    <input type="text" name="image" class="form-contro" id="exampleFormControlInput1"
+                                        value="{{ $event -> image }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Fecha del evento</label>
+                                    <input type="date" name="date" class="form-contro" id="exampleFormControlInput1"
+                                        value="{{ $event -> date }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Hora del evento</label>
+                                    <input type="time" name="start_hour" class="form-contro"
+                                        id="exampleFormControlInput1" value="{{ $event -> start_hour }}">
+                                </div>
+
+                                <div class="d-flex flex-row bd-highlight mb-3 p-4">
+                                    <a class="btn btn-primary"
+                                        href="{{ route('showEvent', $event -> id) }}">Cancelar</a>
+                                    <button type="submit" class="btn btn-outline-success" value="Create"
+                                        onclick="return confirm('¿Estás seguro de querer modificar este evento? {{$event->name}}')">Aceptar</button>
+                                </div>
+
+
+                        </div>
+
+                    </div>
+                    </form>
+
+
+                </div>
+            </div>
+
 
 
 
@@ -152,99 +164,100 @@
 @endsection
 
 
-                @endif
+@endif
 
 
 @if (Auth::check())
 
 
-            @foreach ($eventsuscribe as $eventsubcribed )
-            @if ($event->id == $eventsubcribed->pivot->event_id)
-            @section('content')
-            <div class="card">
+@foreach ($eventsuscribe as $eventsubcribed )
+@if ($event->id == $eventsubcribed->pivot->event_id)
+@section('content')
+<div class="card">
 
-                <div class="card-img-overlay">
-                    <h5 class="card-title">Evento:</h5>
-                    <p class="card-title">{{$event -> title}}</p>
-                    <h5 class="card-title">Descripción:</h5>
-                    <p class="card-text">{{ $event -> description }}</p>
-                    <h5 class="card-title">Comienzo:</h5>
+    <div class="card-img-overlay">
+        <h5 class="card-title">Evento:</h5>
+        <p class="card-title">{{$event -> title}}</p>
+        <h5 class="card-title">Descripción:</h5>
+        <p class="card-text">{{ $event -> description }}</p>
+        <h5 class="card-title">Comienzo:</h5>
 
-                    <div class="d-flex gap-5">
-                        <h5 class="card-title">Fecha Inicio:</h5>
-                        <h5 class="card-title">Hora:</h5>
-                    </div>
+        <div class="d-flex gap-5">
+            <h5 class="card-title">Fecha Inicio:</h5>
+            <h5 class="card-title">Hora:</h5>
+        </div>
 
-                    <div class="d-flex gap-5">
-                        <p class="card-text">{{ $event -> date }}</p>
-                        <p class="card-text">{{ $event -> start_hour }}</p>
-                    </div>
+        <div class="d-flex gap-5">
+            <p class="card-text">{{ $event -> date }}</p>
+            <p class="card-text">{{ $event -> start_hour }}</p>
+        </div>
 
-                    <div class="d-flex gap-3">
-                        <img src="{{ URL('/img/Turnouticon.png')}}">
-                        <p class="card-text">{{ $event -> sub_people }}/{{ $event -> total_people }}</p>
-                    </div>
-
-
+        <div class="d-flex gap-3">
+            <img src="{{ URL('/img/Turnouticon.png')}}">
+            <p class="card-text">{{ $event -> sub_people }}/{{ $event -> total_people }}</p>
+        </div>
 
 
 
-                    <button type="button" class="btn btn-secondary"><a href="{{ route('unscribeEvent', $event->id) }}"
-                            onclick="return confirm('¿Estás seguro de querer desapuntarse de este evento? {{$event->name}} -ID {{ $event -> id }}')">Desinscribirse</a></button>
 
 
-                </div>
+        <button type="button" class="btn btn-secondary"><a href="{{ route('unscribeEvent', $event->id) }}"
+                onclick="return confirm('¿Estás seguro de querer desapuntarse de este evento? {{$event->name}} -ID {{ $event -> id }}')">Desinscribirse</a></button>
 
-            </div>
-            @endsection
-            @break
+
+    </div>
+
+</div>
+@endsection
+@break
+@endif
+@endforeach
+@endif
+
+@section('content')
+<div class="card">
+
+    <div class="card-img-overlay">
+        <h5 class="card-title">Evento:</h5>
+        <p class="card-title">{{$event -> title}}</p>
+        <h5 class="card-title">Descripción:</h5>
+        <p class="card-text">{{ $event -> description }}</p>
+        <h5 class="card-title">Comienzo:</h5>
+
+        <div class="d-flex gap-5">
+            <h5 class="card-title">Fecha Inicio:</h5>
+            <h5 class="card-title">Hora:</h5>
+        </div>
+
+        <div class="d-flex gap-5">
+            <p class="card-text">{{ $event -> date }}</p>
+            <p class="card-text">{{ $event -> start_hour }}</p>
+        </div>
+
+        <div class="d-flex gap-3">
+            <img src="{{ URL('/img/Turnouticon.png')}}">
+            <p class="card-text">{{ $event -> sub_people }}/{{ $event -> total_people }}</p>
+        </div>
+
+
+
+
+
+        @if(($event -> sub_people)>=($event -> total_people))
+        <button type="button" class="btn btn-primary" onclick="return confirm('Evento completo')"
+            id="asist-button">Asistir</button>
+        @endif
+
+        @if(($event -> sub_people)<($event -> total_people))
+
+            <a href="{{ route('inscribeEvent', ['id'=>$event->id]) }}"><button type="button"
+                    class="btn btn-primary buttonAsist text-white" onclick="('Evento completo')"
+                    id="asist-button">Asistir</button></a>
+
             @endif
-            @endforeach
-            @endif
 
-            @section('content')
-            <div class="card">
+    </div>
 
-                <div class="card-img-overlay">
-                    <h5 class="card-title">Evento:</h5>
-                    <p class="card-title">{{$event -> title}}</p>
-                    <h5 class="card-title">Descripción:</h5>
-                    <p class="card-text">{{ $event -> description }}</p>
-                    <h5 class="card-title">Comienzo:</h5>
+</div>
 
-                    <div class="d-flex gap-5">
-                        <h5 class="card-title">Fecha Inicio:</h5>
-                        <h5 class="card-title">Hora:</h5>
-                    </div>
-
-                    <div class="d-flex gap-5">
-                        <p class="card-text">{{ $event -> date }}</p>
-                        <p class="card-text">{{ $event -> start_hour }}</p>
-                    </div>
-
-                    <div class="d-flex gap-3">
-                        <img src="{{ URL('/img/Turnouticon.png')}}">
-                        <p class="card-text">{{ $event -> sub_people }}/{{ $event -> total_people }}</p>
-                    </div>
-
-
-
-
-                    @if(($event -> sub_people)>=($event -> total_people))
-                    <button type="button" class="btn btn-primary" onclick="return confirm('Evento completo')"
-                        id="asist-button">Asistir</button>
-                    @endif
-
-                    @if(($event -> sub_people)<($event -> total_people))
-
-                        <a href="{{ route('inscribeEvent', ['id'=>$event->id]) }}"><button type="button"
-                                class="btn btn-primary buttonAsist text-white" onclick="('Evento completo')"
-                                id="asist-button">Asistir</button></a>
-
-                        @endif
-
-                </div>
-
-            </div>
-
-            @endsection
+@endsection
