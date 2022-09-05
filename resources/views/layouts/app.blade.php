10b5c67d037ebbe89916f23f3c6e3cca145d1074
dani martin <danimartinjuarez@gmail.com>
2022-09-05 13:49:10 +0200

    audio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,9 @@
 			@yield('carousel')
 			@yield('content')
 		</main>
+        <div class="d-flex justify-content-center">
+        <audio src="{{url('/audio/intro.mp3')}}" controls autoplay></audio>
+    			</div>
 		<script src="{{ asset('JavaScript/carousel.js') }}" type="text/javascript"></script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
